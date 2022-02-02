b96dbde557e52aca2cde55b80d068e5d2e7d15c0
Emad <95617246+Emad02@users.noreply.github.com>
2022-02-02 13:05:12 +0330

    Add files via upload

---
+++ tickt_seller.py
@@ -0,0 +1,45 @@
+# procesing of ticket value
+
+b = int(input("enter your age: "))
+if b > 3:
+    b = 1
+elif b <= 3:
+    b = 0
+else:
+    print('your input is not a int, please add a number.')
+
+a2 = int(input("enter your age: "))
+if a2 > 3:
+    a2 = 1
+elif a2 <= 3:
+    a2 = 0
+else:
+    print('your input is not a int, please add a number.')
+
+a3 = int(input("enter your age: "))
+if a3 > 3:
+    a3 = 1
+elif a3 <= 3:
+    a3 = 0
+else:
+    print('your input is not a int, please add a number.')
+
+a4 = int(input("enter your age: "))
+if a4 > 3:
+    a4 = 1
+elif a4 <= 3:
+    a4 = 0
+else:
+    print('your input is not a int, please add a number.')
+
+a5 = int(input("enter your age: "))
+if a5 > 3:
+    a5 = 1
+elif a5 <= 3:
+    a5 = 0
+else:
+    print('your input is not a int, please add a number.')
+
+s = b + a2 + a3 + a4 + a5
+v = s * 100
+print(v)
